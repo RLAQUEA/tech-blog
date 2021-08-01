75ca0d3e55dad34e1b56003a9fae00f5e1a7fc5b
rlaquea <rlaquea14@gmail.com>
2021-08-01 17:52:37 -0400

    started outline for User, Comment & Post models

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// Post.belongsTo(User, {
+//     foreignKey: 'id'
+// }),
+
+// Post.hasMany(Comment, {
+//     foreignKey: ''
+// },
+
+// Comment.belongsTo(User, {
+//     foreignKey: 'id'
+
+// })
